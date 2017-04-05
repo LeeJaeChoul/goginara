4f71c203f315014966004245509b5cd6852aa9c9
Eric <spb828@naver.com>
2017-04-05 19:35:06 +0900

    a

--- app/controllers/box_controller.rb
+++ app/controllers/box_controller.rb
@@ -1,6 +1,13 @@
 class BoxController < ApplicationController
+  # before_action :is_signed?
+
+  def index
+    # @boxes = User.boxes
+    @boxes = Box.all
+  end
 
   def new
-    
+
   end
+  
 end
